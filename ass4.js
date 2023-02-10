11539e42ed937e8844ff65bb9f5ec6e4d3ca28df
Reach <2022298somnang@aupp.edu.kh>
2023-02-10 11:38:41 +0700

    test

---
+++ ass4.js
@@ -0,0 +1,36 @@
+// var output = sumRange(4)
+// console.log(output);
+
+// // function sumRange(num){
+// // 	if(num == 1) return 1;
+
+// // 	return num + sumRange(num - 1);
+// // }
+
+// // var result = remainder(5,5);
+// // console.log(result);
+
+// // function remainder(num1,num2){
+// //     return num1 % num2;
+// // }
+
+// var result = giveMeSomething("is better than nothing") ;
+// console.log(result);
+
+// function giveMeSomething(_x){
+//     return "something" + " " + _x;
+// }
+
+// var result = isSameNum(8, "8");
+// console.log(result);
+
+// function isSameNum(num1, num2) {
+// 	return num1 === num2 ? true : false
+// }
+
+var result = ageCal(2004);
+console.log(result);
+
+function ageCal(n) {
+	return 2023-n;
+}
